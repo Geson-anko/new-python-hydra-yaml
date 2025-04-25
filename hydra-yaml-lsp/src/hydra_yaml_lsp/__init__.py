397eaf199efdbf49a1f2705b0411486ba3d18c6f
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-04-25 15:57:47 +0900

    setup formatter and linter

--- hydra-yaml-lsp/src/hydra_yaml_lsp/__init__.py
+++ hydra-yaml-lsp/src/hydra_yaml_lsp/__init__.py
@@ -1,4 +1,9 @@
+from importlib import metadata
+
 from hydra_yaml_lsp._core import hello_from_bin
+
+__version__ = metadata.version(__name__.replace("_", "-"))
+
 
 def hello() -> str:
     return hello_from_bin()
---
+++ hydra-yaml-lsp/tests/test_package.py
@@ -0,0 +1,13 @@
+import tomllib
+from pathlib import Path
+
+import hydra_yaml_lsp
+
+PROJECT_ROOT = Path(__file__).parent.parent
+
+
+def test_version():
+    with open(PROJECT_ROOT / "pyproject.toml", "rb") as f:
+        pyproject = tomllib.load(f)
+
+    assert hydra_yaml_lsp.__version__ == pyproject["project"]["version"]
